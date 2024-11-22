e2c3d79af6b56f18f29259e4064c934210ead011
Muhd-Saifullah-dev <saifullahdev98@gmail.com>
2024-11-22 22:53:25 +0500

    add prettier format and make user model and some updating

--- config/config.js
+++ config/config.js
@@ -5,5 +5,10 @@ require("dotenv").config({
 })
 
 module.exports={
-    MONGO_URI:process.env.MONGO_URI
+    MONGO_URI:process.env.MONGO_URI,
+    PORT:process.env.PORT,
+    ACCESS_TOKEN_SECRET:process.env.ACCESS_TOKEN_SECRET,
+    ACCESS_TOKEN_EXPIRY:process.env.ACCESS_TOKEN_EXPIRY,
+    REFRESH_TOKEN_SECRET:process.env.REFRESH_TOKEN_SECRET,
+    REFRESH_TOKEN_EXPIRY:process.env.REFRESH_TOKEN_EXPIRY,
 }
